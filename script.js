8147df9e7ff68ff5b3c8da2f0b61a11636de1ebe
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-06 17:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let form = document.getElementById("form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+	let input1 = document.getElementById("first-name").value;
+	let input2 = document.getElementById("last-name").value;
+	let input3 = document.getElementById("number").value;
+	let input4 = document.getElementById("mail").value;
+	let ans = `First Name: ${input1} Last Name: ${input2} Phone Number: ${input3} Email ID: ${input4}`
+    alert(ans);
+});
+
